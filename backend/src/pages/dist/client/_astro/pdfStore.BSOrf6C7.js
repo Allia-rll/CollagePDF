8ccef27e6c06e6e5daff61e3b46a00c1e212e932
lucad-28 <lucadev.rll28@gmail.com>
2024-07-24 17:47:08 -0500

    fix api-route in production

--- backend/src/pages/dist/client/_astro/pdfStore.BSOrf6C7.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index.BzUbq9mA.js";const t=e(r=>({pdfUrl:null,setPdfUrl:l=>r(()=>({pdfUrl:URL.createObjectURL(l)})),cleanPdfUrl:()=>r(()=>({pdfUrl:null}))}));export{t as u};
---
+++ backend/src/pages/dist/client/_astro/pdfStore.DoXvm6o2.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index.22bC0alc.js";const t=e(r=>({pdfUrl:null,setPdfUrl:l=>r(()=>({pdfUrl:URL.createObjectURL(l)})),cleanPdfUrl:()=>r(()=>({pdfUrl:null}))}));export{t as u};
